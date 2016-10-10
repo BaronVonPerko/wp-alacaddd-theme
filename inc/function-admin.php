938f475335a7b36738223ac34c03312550531917
Chris Perko <Chris Perko>
2016-10-10 13:37:39 -0500

    Added social media settings

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -38,8 +38,15 @@ add_action('admin_menu', 'perko_add_admin_page');
 
 function perko_custom_settings() {
   register_setting('perko-settings-group', 'first_name');
+  register_setting('perko-settings-group', 'last_name');
+  register_setting('perko-settings-group', 'twitter', 'perko_sanitize_twitter');
+  register_setting('perko-settings-group', 'facebook');
+
   add_settings_section('perko-sidebar-options', 'Sidebar Options', 'perko_sidebar_options', 'perko_theme');
-  add_settings_field('sidebar-name', 'First Name', 'perko_sidebar_name', 'perko_theme', 'perko-sidebar-options');
+
+  add_settings_field('sidebar-name', 'Name', 'perko_sidebar_name', 'perko_theme', 'perko-sidebar-options');
+  add_settings_field('sidebar-twitter', 'Twitter', 'perko_sidebar_twitter', 'perko_theme', 'perko-sidebar-options');
+  add_settings_field('sidebar-facebook', 'Facebook', 'perko_sidebar_facebook', 'perko_theme', 'perko-sidebar-options');
 }
 
 function perko_sidebar_options() {
@@ -48,9 +55,34 @@ function perko_sidebar_options() {
 
 function perko_sidebar_name() {
   $firstName = esc_attr(get_option('first_name'));
+  $lastName = esc_attr(get_option('last_name'));
   echo '<input type="text" name="first_name" value="' . $firstName . '" placeholder="First Name" />';
+  echo '<input type="text" name="last_name" value="' . $lastName . '" placeholder="Last Name" />';
 }
 
+function perko_sidebar_twitter() {
+  $twitter = esc_attr(get_option('twitter'));
+  echo '<input type="text" name="twitter" value="' . $twitter . '" placeholder="Twitter" />';
+  echo '<p class="description">Input your Twitter handle without the @ character.</p>';
+}
+
+function perko_sidebar_facebook() {
+  $facebook = esc_attr(get_option('facebook'));
+  echo '<input type="text" name="facebook" value="' . $facebook . '" placeholder="Facebook" />';
+}
+
+
+
+// Sanitization Settings
+function perko_sanitize_twitter($val) {
+  $val = sanitize_text_field($val);
+  $val = str_replace('@', '', $val);
+  return $val;
+}
+
+
+
+// Build Admin Sub Pages
 function perko_theme_create_page() {
   require_once(get_template_directory() . '/inc/templates/perko-admin.php');
 }
