3fe9c207bf694fe819e97780aa272a10f0e14e5a
Chris Perko <Chris Perko>
2016-10-10 13:07:25 -0500

    Added first name setting

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -30,9 +30,26 @@ function perko_add_admin_page() {
                    'manage_options',
                    'perko_theme_css',
                    'perko_theme_create_css_page');
+
+  // Activate custom settings
+  add_action('admin_init', 'perko_custom_settings');
 }
 add_action('admin_menu', 'perko_add_admin_page');
 
+function perko_custom_settings() {
+  register_setting('perko-settings-group', 'first_name');
+  add_settings_section('perko-sidebar-options', 'Sidebar Options', 'perko_sidebar_options', 'perko_theme');
+  add_settings_field('sidebar-name', 'First Name', 'perko_sidebar_name', 'perko_theme', 'perko-sidebar-options');
+}
+
+function perko_sidebar_options() {
+  echo 'Customize your sidebar information';
+}
+
+function perko_sidebar_name() {
+  $firstName = esc_attr(get_option('first_name'));
+  echo '<input type="text" name="first_name" value="' . $firstName . '" placeholder="First Name" />';
+}
 
 function perko_theme_create_page() {
   require_once(get_template_directory() . '/inc/templates/perko-admin.php');
